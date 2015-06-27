9f413b8641e3ae4e87af74ab05c7c50104a38bf9
Danzante <tim-95@narod.ru>
2015-06-27 13:52:15 +0300

    Now Checking CLick on Me Right
    Found how to make mouse position to screen coordinates.
    Dragging:
    http://answers.unity3d.com/questions/12322/drag-gameobject-with-mouse.html
    ???

--- QB - TCG/Assets/UserControl.cs
+++ QB - TCG/Assets/UserControl.cs
@@ -2,15 +2,36 @@
 using System.Collections;
 
 public class UserControl : MonoBehaviour {
-
-	bool IsTargeted;
+	
+	private Vector3 MP;//  mouse position
+	private Vector3 SMP;// screen mouse position
+	private Vector3 Scale;
+	private Vector3 teststep = new Vector3(1, 0, 0);
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	private bool IsMouseIn(){
+		SMP = Camera.main.ScreenToWorldPoint(MP);
+		Scale = transform.lossyScale;
+		if (SMP.x < transform.position.x + Scale.x/2)
+			if (SMP.x > transform.position.x - Scale.x/2)
+				if (SMP.y > transform.position.y - Scale.y/2)
+					if (SMP.y < transform.position.y + Scale.y/2)
+						return true;
+		return false;
+	}
+
+	private bool IsTargeted(){
+		MP = Input.mousePosition;
+		return Input.GetKey (KeyCode.Mouse0) && IsMouseIn();
+	}
+
 	// Update is called once per frame
 	void Update () {
+		if (IsTargeted())
+			transform.position = transform.position + teststep;
 	}
 }
